5ed4e0519aeef53c0800798b89e1d6e02255b223
Mike DiDomizio <mikedidomizio@gmail.com>
2021-04-11 14:52:55 -0400

    Update header navigation items

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -23,17 +24,53 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleExport = () => {
+        // todo need to move the board contents up
+        handleClose();
+    };
+
+    const handleImport = () => {
+        handleClose();
+    };
+
+    const handleHelp = () => {
+        // todo
+        handleClose();
+    };
 
     return (
         <div className="App">
             <div className={classes.root}>
                 <AppBar position="static">
                     <Toolbar>
-                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"
+                                    onClick={handleClick}>
                             <MenuIcon/>
                         </IconButton>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleExport}>Export</MenuItem>
+                            <MenuItem onClick={handleImport}>Import</MenuItem>
+                            <MenuItem onClick={handleHelp}>Help</MenuItem>
+                        </Menu>
+
                         <Typography variant="h6" className={classes.title}>
-                            Home
+                            Chrome Auto Grouping
                         </Typography>
                     </Toolbar>
                 </AppBar>
