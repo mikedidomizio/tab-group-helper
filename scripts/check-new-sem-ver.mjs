17c343196d104275331051357ca52b395735e92a
Mike DiDomizio <mikedidomizio@gmail.com>
2021-10-30 11:09:30 -0400

    Add scripts to upload/publish to Chrome webstore

---
+++ scripts/check-new-sem-ver.mjs
@@ -0,0 +1,16 @@
+/**
+ * Takes two versions and returns whether the new version is increased either major, minor or patch
+ * @param semVer current sem ver
+ * @param newSemVer new sem ver to check
+ * @returns {boolean}
+ */
+const checkNewSemVerIsGreater = (semVer, newSemVer) => {
+  const [major, minor, patch] = semVer.split('.').map(i => parseInt(i, 10));
+  const [newMajor, newMinor, newPatch] = newSemVer.split('.').map(i => parseInt(i, 10));
+
+  return !!(newMajor > major | newMinor > minor | newPatch > patch);
+}
+
+export {
+  checkNewSemVerIsGreater
+}
