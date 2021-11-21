512fa0142d8fd6f9314a5d14dfeb2597e6725935
Mike DiDomizio <mikedidomizio@gmail.com>
2021-11-20 20:16:05 -0500

    Add unit tests for background page

---
+++ scripts/run-all-tests-for-coverage.mjs
@@ -0,0 +1,23 @@
+import { execSync } from 'child_process';
+
+// the CI environment, even when reading the coverage-puppeteer file/folder it won't show up in codecov
+// tried sending as LCOV, JSON, merging the files together.  Days lost.
+
+// Run the Puppeteer style tests, the coverage is output to the temp .nyc_output folder
+await execSync('npm run test -- general.spec.ts');
+
+// the Puppeteer style tests we convert to JSON
+await execSync('npx nyc report --reporter=json --report-dir=./coverage-puppeteer');
+
+// RTL/unit tests
+await execSync('npm run test:coverage -- --coverageReporters=json --coverageDirectory=./coverage-rtl --testPathIgnorePatterns="general.*" --coveragePathIgnorePatterns="general.*"');
+
+// we expect that we have both coverage suites
+// we merge them together and output them to a JSON file
+await execSync('npx istanbul-merge --out ./coverage.json ./coverage-rtl/coverage-final.json ./coverage-puppeteer/coverage-final.json');
+
+// convert the JSON to HTML
+// for some reason GitHub actions can't run this command therefore we only run it coverage locally
+if (!process.env.CI) {
+  await execSync('npx istanbul report --include ./coverage.json --dir coverage html');
+}
