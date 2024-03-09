3066e517a8ffff70c91915f3e1925141d382b32e
Mike DiDomizio <mikedidomizio@gmail.com>
2024-03-09 08:49:32 -0500

    Add GCP client secret

--- scripts/chrome-web-store-api.mjs
+++ scripts/chrome-web-store-api.mjs
@@ -1,10 +1,11 @@
 import chromeWebstoreUpload from 'chrome-webstore-upload';
 
-const { GCP_CLIENT_ID, GCP_REFRESH_TOKEN } = process.env;
+const { GCP_CLIENT_ID, GCP_CLIENT_SECRET, GCP_REFRESH_TOKEN } = process.env;
 
 const chromeWebStore = chromeWebstoreUpload({
   extensionId: 'llhkcebnebfiaamifhbpehjompplpnae',
   clientId: GCP_CLIENT_ID,
+  client_secret: GCP_CLIENT_SECRET,
   refreshToken: GCP_REFRESH_TOKEN,
 });
 
