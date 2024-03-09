ed287fc2e25109f889605178848ec1a856c26382
Mike DiDomizio <mikedidomizio@gmail.com>
2024-03-09 08:54:15 -0500

    Better error logging

--- scripts/chrome-web-store-api.mjs
+++ scripts/chrome-web-store-api.mjs
@@ -2,6 +2,18 @@ import chromeWebstoreUpload from 'chrome-webstore-upload';
 
 const { GCP_CLIENT_ID, GCP_CLIENT_SECRET, GCP_REFRESH_TOKEN } = process.env;
 
+if (!GCP_CLIENT_ID) {
+  throw new Error('No GCP_CLIENT_ID')
+}
+
+if (!GCP_CLIENT_SECRET) {
+  throw new Error('No GCP_CLIENT_SECRET')
+}
+
+if (!GCP_REFRESH_TOKEN) {
+  throw new Error('No GCP_REFRESH_TOKEN')
+}
+
 const chromeWebStore = chromeWebstoreUpload({
   extensionId: 'llhkcebnebfiaamifhbpehjompplpnae',
   clientId: GCP_CLIENT_ID,
