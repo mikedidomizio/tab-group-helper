31fcc2caa7c717557a0062d7f3a722a06453df6c
Mike DiDomizio <mikedidomizio@gmail.com>
2024-03-09 09:04:12 -0500

    Make Codacy happy

--- scripts/chrome-web-store-api.mjs
+++ scripts/chrome-web-store-api.mjs
@@ -3,15 +3,15 @@ import chromeWebstoreUpload from 'chrome-webstore-upload';
 const { GCP_CLIENT_ID, GCP_CLIENT_SECRET, GCP_REFRESH_TOKEN } = process.env;
 
 if (!GCP_CLIENT_ID) {
-  throw new Error('No GCP_CLIENT_ID')
+  throw new Error('No GCP_CLIENT_ID');
 }
 
 if (!GCP_CLIENT_SECRET) {
-  throw new Error('No GCP_CLIENT_SECRET')
+  throw new Error('No GCP_CLIENT_SECRET');
 }
 
 if (!GCP_REFRESH_TOKEN) {
-  throw new Error('No GCP_REFRESH_TOKEN')
+  throw new Error('No GCP_REFRESH_TOKEN');
 }
 
 const chromeWebStore = chromeWebstoreUpload({
